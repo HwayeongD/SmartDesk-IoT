e113a035b3cd2c402d94711f0024a081d27434dd
wocjs <wocjs602@gmail.com>
2023-07-21 17:49:25 +0900

    [Update] Arduino

--- Arduino/0721/0721.ino
+++ Arduino/0721/0721.ino
@@ -1,79 +1,46 @@
 #include <ESP8266WiFi.h>
- 
-#define PIN_LED D1
- 
+#include <WiFiClient.h>
+
 const char* ssid = "MULTI_GUEST";
 const char* password = "guest1357";
- 
-WiFiServer server(80);
- 
+const char* serverURL = "http://your-server-url.com/api/data"; // Replace with your server URL
+
 void setup() {
-  pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, LOW);
-  
   Serial.begin(115200);
-  
-  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
- 
+
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
   }
-  Serial.println("");
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  
-  server.begin();
-  Serial.println("Server started");
+
+  Serial.println("Connected to WiFi!");
 }
- 
+
 void loop() {
-  WiFiClient client = server.available();
-  if(!client) return;
-  
-  Serial.println("새로운 클라이언트");
-  client.setTimeout(5000);
-  
-  String request = client.readStringUntil('\r');
-  Serial.println("request: ");
-  Serial.println(request);
- 
-  if(request.indexOf("/ledoff") != -1) {
-    digitalWrite(PIN_LED, LOW);
+  if (WiFi.status() == WL_CONNECTED) {
+    WiFiClient client;
+
+    Serial.print("Connecting to server: ");
+    Serial.println(serverURL);
+
+    if (client.connect(serverURL, 80)) {
+      Serial.println("Connected to server!");
+      client.print("GET / HTTP/1.1\r\n");
+      client.print("Host: ");
+      client.print(serverURL);
+      client.print("\r\n\r\n");
+
+      while (client.available()) {
+        String line = client.readStringUntil('\r');
+        Serial.print(line);
+      }
+
+      client.stop();
+    } else {
+      Serial.println("Connection to server failed.");
+    }
+
+    delay(5000); // Wait for 5 seconds before the next request
   }
-  else if(request.indexOf("/ledon") != -1) {
-    digitalWrite(PIN_LED, HIGH);
-  }
-  else {
-    Serial.println("invalid request");
-    digitalWrite(PIN_LED, digitalRead(PIN_LED));
-  }
- 
-  while(client.available()) {
-    client.read();
-  }
- 
-  client.print("HTTP/1.1 200 OK");
-  client.print("Content-Type: text/html\r\n\r\n");
-  client.print("<!DOCTYPE HTML>");
-  client.print("<html>");
-  client.print("<head>"); 
-  client.print("<meta&nbsp;charset=\"UTF-8\">");
-  client.print("<title>LED CONTROL</title>");
-  client.print("</head>");
-  client.print("<body>");
-  client.print("<h2>HANIUM LED Control Page</h2>");
-  client.print("<a href='/ledon'>LED ON</a>");
-  client.print("<br>");
-  client.print("<a href='/ledoff'>LED OFF</a>");
-  client.print("<br>");
-  client.print("LED Status : ");
-  client.print((digitalRead(PIN_LED)) ? "ON" : "OFF");
-  client.print("</body>");
-  client.print("</html>");
- 
-  Serial.println("클라이언트 연결 해제");
 }
--- Arduino/esp/esp.ino
+++ Arduino/esp/esp.ino
@@ -5,8 +5,8 @@ int TX = 2;
 SoftwareSerial ESP01(RX, TX); 
 
 void setup() { 
-  Serial.begin(74880);  //아두이노 우노 보드레이트 9600
-  ESP01.begin(74880);   //와이파이 모듈 보드레이트 115200 / 74880
+  Serial.begin(115200);  //아두이노 우노 보드레이트 9600
+  ESP01.begin(115200);   //와이파이 모듈 보드레이트 115200 / 74880
   ESP01.setTimeout(5000); 
   delay(1000);
   Serial.print("Start");
@@ -14,7 +14,7 @@ void setup() {
 
 void loop() { 
   if (Serial.available()){
-    ESP01.write(Serial.read()); 
+    ESP01.write(ESP01.read()); 
   } 
   if (ESP01.available()) { 
     Serial.write(ESP01.read()); 
