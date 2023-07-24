5d04d2b5a46f2037c063213680a304e0d4d6e95b
wocjs <wocjs602@gmail.com>
2023-07-24 17:38:01 +0900

    [📰test] Arduino Server & Examples

--- Arduino/Examples/WiFiWebClient/WiFiWebClient.ino
+++ Arduino/Examples/WiFiWebClient/WiFiWebClient.ino
@@ -1,6 +1,6 @@
-#include "WiFiS3.h"
-// #include "WebSockets.h"
-#include "arduino_secrets.h" 
+#include <WiFiS3.h>
+#include <WebSocketsClient.h>
+#include "arduino_secrets.h"
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
@@ -8,111 +8,94 @@ char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as k
 int keyIndex = 0;            // your network key index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "192.168.171.6";    // name address for Google (using DNS)
-int port = 8000;
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
-WiFiClient client;
+char server[] = "ws://192.168.171.2";    // WebSocket server address
+int port = 8080;
+
+WiFiClient wifiClient;
+WebSocketsClient webSocket;
 
 /* -------------------------------------------------------------------------- */
 void setup() {
-/* -------------------------------------------------------------------------- */  
-  //Initialize serial and wait for port to open:
+/* -------------------------------------------------------------------------- */
+  // Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  
-  // check for the WiFi module:
+
+  // Check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
-    // don't continue
+    // Don't continue
     while (true);
   }
-  
+
   String fv = WiFi.firmwareVersion();
   if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
     Serial.println("Please upgrade the firmware");
   }
-  
-  // attempt to connect to WiFi network:
+
+  // Attempt to connect to WiFi network:
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
     status = WiFi.begin(ssid, pass);
-     
-    // wait 10 seconds for connection:
+
+    // Wait 10 seconds for connection:
     delay(10000);
   }
-  
-  printWifiStatus();
- 
-  Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
-  if (client.connect(server, port)) {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.println("GET /tables/list/ HTTP/1.1");
-    client.println("Host: 192.168.171.6:8000");
-    client.println("Connection: close");
-    client.println();
-  }
-}
 
-/* just wrap the received data up to 80 columns in the serial print*/
-/* -------------------------------------------------------------------------- */
-void read_response() {
-/* -------------------------------------------------------------------------- */  
-  uint32_t received_data_num = 0;
-  while (client.available()) {
-    /* actual data reception */
-    char c = client.read();
-    /* print data to serial port */
-    Serial.print(c);
-    /* wrap data to 80 columns*/
-    received_data_num++;
-    if(received_data_num % 80 == 0) { 
-      Serial.println();
-    }
-  }  
+  printWifiStatus();
+
+  // Connect to the WebSocket server
+  webSocket.begin(server, port, "/");  // Connect to the WebSocket server
+  webSocket.onEvent(webSocketEvent);   // Set up event handler for WebSocket events
 }
 
 /* -------------------------------------------------------------------------- */
 void loop() {
-/* -------------------------------------------------------------------------- */  
-  read_response();
+/* -------------------------------------------------------------------------- */
+  // Maintain the WebSocket connection
+  webSocket.loop();
+}
 
-  // if the server's disconnected, stop the client:
-  if (!client.connected()) {
-    Serial.println();
-    Serial.println("disconnecting from server.");
-    client.stop();
-
-    // do nothing forevermore:
-    while (true);
+/* -------------------------------------------------------------------------- */
+void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
+/* -------------------------------------------------------------------------- */
+  switch (type) {
+    case WStype_DISCONNECTED:
+      Serial.println("Disconnected from WebSocket server");
+      break;
+    case WStype_CONNECTED:
+      Serial.println("Connected to WebSocket server");
+      break;
+    case WStype_TEXT:
+      // Handle incoming text data from the WebSocket server
+      Serial.print("Received data: ");
+      for (size_t i = 0; i < length; i++) {
+        Serial.print((char)payload[i]);
+      }
+      Serial.println();
+      break;
   }
 }
 
 /* -------------------------------------------------------------------------- */
 void printWifiStatus() {
-/* -------------------------------------------------------------------------- */  
-  // print the SSID of the network you're attached to:
+/* -------------------------------------------------------------------------- */
+  // Print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
+  // Print your board's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  // print the received signal strength:
+  // Print the received signal strength:
   long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
+  Serial.print("Signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
 }
